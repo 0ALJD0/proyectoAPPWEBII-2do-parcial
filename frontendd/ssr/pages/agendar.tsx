6d85779d760207ec006d59096b9c7d98939a888d
YandyMC <yandymc016@gmail.com>
2022-07-18 17:04:37 -0500

    cosas

---
+++ frontendd/ssr/pages/agendar.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head';
+import React  from 'react'
+
+
+const Registrar: NextPage= () =>{
+    return (<>
+        <Head>
+            <h1>Agende su cita aqui</h1>
+        </Head>
+    </>
+    );
+}
+
+
+export default Registrar
