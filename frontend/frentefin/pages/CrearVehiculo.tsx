8224f947ad2d88d48974c237f02d3cd8fb9d60f5
YandyMC <yandymc016@gmail.com>
2022-07-19 15:17:16 -0500

    componente de crear cliente y vehiculo, CSS en proceso

--- frontend/frentefin/pages/CrearVehiculo.tsx
+++ frontend/frentefin/pages/CrearVehiculo.tsx
@@ -16,22 +16,23 @@
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossOrigin="anonymous" />
             </Head>
             <main>
-                <div className='container'>
+                <div className='container-fluid'>
                     <div className='row'>
                         <form action="">
-                            <div>
+                            <div className='col-md-5'>
                                 <label htmlFor="">Placa </label>
                                 <input className='form-control' type="text" name="placa" id="placa" />
                             </div>
-                            <div>
+                            <input type="hidden" name="idcliente" id="idcliente"/>
+                            <div className='col-md-5'>
                                 <label htmlFor="">Marca</label>
                                 <input className='form-control' type="text" name="marca" id="marca" />
                             </div>
-                            <div>
+                            <div className='col-md-5'>
                                 <label htmlFor="">Color</label>
                                 <input className='form-control' type="text" name="color" id="color" />
                             </div>
-                            <div>
+                            <div className='col-md-5'>
                                 <label htmlFor="">Modelo</label>
                                 <input className='form-control' type="text" name="modelo" id="modelo" />
                             </div>
--- frontend/frentefin/pages/RegistroCliente.tsx
+++ frontend/frentefin/pages/RegistroCliente.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-//import  from 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.css'
 
 
 //Parte hecha con client side render
@@ -10,33 +8,34 @@ const RCliente: NextPage = () => {
         <>
         <Head>
             <title>Registro de Usuario</title>
-            
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossOrigin="anonymous" />
         </Head>
         <main>
-        <h1>Ingrese sus datos</h1>
-        <div className='@media (min-width: @screen-sm-min)'>
-            <div className='row'>
-            <form action="">
-            <div className='mb-3'>
-                <label className='formfile' htmlFor="first">Nombre:</label>
-                <input className='form-control' type="text" id="nombre" name="nombre" />
+            <h1 className='display-4 text-center'>Ingrese sus datos</h1>
+        <div className='container'>
+            <div className='row bg-dark text-white '>
+                <form action="">
+                <div className='col-md-4  col-md-offset-3' >
+                    <label className='formfile' htmlFor="first">Nombre:</label>
+                    <input className='form-control' type="text" id="nombre" name="nombre" />
+                </div>
+                <div className='col-md-4'>
+                    <label className='formfile' htmlFor="last">Correo:</label>
+                    <input className='form-control' type="email" id="correo" name="correo" />    
+                </div>
+                <div className='col-md-4'>
+                    <label className='formfile' htmlFor="">Cedula</label>
+                    <input className='form-control' type="text" id="cedula" name="cedula" />    
+                </div>
+                <div className='col-md-4'>
+                    <label className='formfile' htmlFor="">Numero Telefonico</label>
+                    <input className='form-control' type="text" id="numero" name="numero" />    
+                </div>
+                    <br />
+                    <button className='btn btn-outline-success' type="submit">Enviar</button>
+                </form>
             </div>
-            <div className='mb-3'>
-                <label className='formfile' htmlFor="last">Correo:</label>
-                <input className='form-control' type="text" id="correo" name="correo" />    
-            </div>
-            <div className='mb-3'>
-                <label className='formfile' htmlFor="">Cedula</label>
-                <input className='form-control' type="text" id="cedula" name="cedula" />    
-            </div>
-            <div className='mb-3'>
-                <label className='formfile' htmlFor="">Numero Telefonico</label>
-                <input className='form-control' type="text" id="numero" name="numero" />    
-            </div>
-                <button className='btn btn-primary' type="submit">Enviar</button>
-        </form>
-            </div>
-        </div>
+        </div>   
         </main>
         </>
     )
