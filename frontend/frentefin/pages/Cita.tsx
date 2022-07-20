83ec1be7f72f58e037d9ad4beedf92bcda783af3
YandyMC <yandymc016@gmail.com>
2022-07-19 20:04:41 -0500

    estructura de citas

--- frontend/frentefin/pages/Cita.tsx
+++ frontend/frentefin/pages/Cita.tsx
@@ -11,6 +11,19 @@ import Head from 'next/head'
 const Crearcita: NextPage = () =>{
     return(
         <>
+        <Head> 
+            <title>Crear vehiculo</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossOrigin="anonymous" />
+        </Head>
+        <section>
+           <div className='container'>
+                <div className='row'>
+                    <form action="">
+                        
+                    </form>
+                </div>
+            </div> 
+        </section>
         </>
     );
 }
